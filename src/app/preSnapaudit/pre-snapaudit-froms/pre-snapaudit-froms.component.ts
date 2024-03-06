450995acc82502dad1ef49a8b1ecdc5af2cf4d9d
Yash Gijabale <yashgijabale@gmail.com>
2024-03-06 11:59:21 +0530

    pre snap audit forms

--- src/app/preSnapaudit/pre-snapaudit-froms/pre-snapaudit-froms.component.ts
+++ src/app/preSnapaudit/pre-snapaudit-froms/pre-snapaudit-froms.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PreSnapauditFromsComponent implements OnInit {
 
+  rating:Array<number> = [0,1,2,3,4,5,6,7,8,9,10]
   constructor() { }
 
   ngOnInit() {
